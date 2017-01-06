75b59ee338cd994445c8a89332acb2ee772a19f6
nyg <nyg>
2017-01-06 15:06:46 +0200

    Refactored code to reconnect automatically if error is received or connection is closed.

--- okcoin-api.js
+++ okcoin-api.js
@@ -1,4 +1,5 @@
 function OKCoin(domain, channels) {
+    console.log('OKCoin object created (' + domain + ')')
     this.wsUrl = 'wss://real.okcoin.' + domain + ':10440/websocket/okcoinapi'
     this.channels = channels
 }
@@ -6,23 +7,46 @@ function OKCoin(domain, channels) {
 OKCoin.prototype.start = function () {
 
     var _this = this
-    this.ws = new WebSocket(this.wsUrl)
 
-    this.ws.onopen = function () {
+    _this.initWs = function () {
 
-        console.log('Connection opened')
+        _this.ws = new WebSocket(_this.wsUrl)
 
-        var channels = []
-        for (channelName in _this.channels) {
-            channels.push({ 'event': 'addChannel', 'channel': channelName })
+        _this.ws.onopen = function (event) {
+
+            console.log('Connection opened')
+
+            var channels = []
+            for (channelName in _this.channels) {
+                channels.push({ 'event': 'addChannel', 'channel': channelName })
+            }
+
+            _this.ws.send(JSON.stringify(channels))
         }
 
-        this.send(JSON.stringify(channels))
+        _this.ws.onmessage = function (event) {
+            JSON.parse(event.data).forEach(function (message) {
+                _this.channels[message.channel](message)
+            })
+        }
+
+        _this.ws.onerror = function (event) {
+
+            console.log('Error received:')
+            console.log(event)
+
+            var status = _this.ws.readyState
+            if (status != 1) {
+                console.log('Connection status was: ' + status + ', restarting connection')
+                _this.initWs()
+            }
+        }
+
+        _this.ws.onclose = function (event) {
+            console.log('Connection closed, restarting connection')
+            _this.initWs()
+        }
     }
 
-    this.ws.onmessage = function (event) {
-        JSON.parse(event.data).forEach(function (message) {
-            _this.channels[message.channel](message)
-        })
-    }
+    _this.initWs()
 }
